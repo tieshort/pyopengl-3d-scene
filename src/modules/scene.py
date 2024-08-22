fd520df4a7d428f0053f20aaad0e955aff750cd6
tieshort <razumoffgleb@gmail.com>
2024-08-22 13:25:17 +0300

    "added mouse callbacks, extended keyboard callback and Camera class"

--- src/modules/scene.py
+++ src/modules/scene.py
@@ -11,23 +11,32 @@ class Scene:
 
         self.background_color = glm.vec3(0)
         self.background_texture = None
-        self.aspect = aspect
 
         self.camera = Camera(glm.vec3(0, -1, 2))
+        self.delta_time = 0.0
+        self.last_frame_time = 0.0
 
-        self.projection_matrix = glm.perspective(glm.radians(90), self.aspect, 0.01, 100.0)
-        self.view_matrix = glm.lookAt(self.camera.position, self.camera.target, self.camera.up)
+        self.fov = 45.0
+        self.aspect = aspect
+        self.near = 0.01
+        self.far = 100.0
 
         # self.projection_matrix = glm.mat4(1)
         # self.view_matrix = glm.mat4(1)
 
-    def render(self, **kwargs: any):
+    def render(self, time: float = 0, **kwargs: any):
+        self.delta_time = time - self.last_frame_time
+        self.last_frame_time = time
         for model in self.objects:
             try:
                 model.render(
-                    projection_matrix = self.projection_matrix,
-                    view_matrix = self.view_matrix,
-                    view_position = self.camera.target,
+                    projection_matrix = glm.perspective(glm.radians(self.fov), 
+                                                        self.aspect,
+                                                        self.near,
+                                                        self.far),
+                    view_matrix = self.camera.get_view_matrix(),
+                    view_position = self.camera.position + self.camera.target,
+                    time = time,
                     dir_lights = self.dirLights,
                     point_lights = self.pointLights,
                     spot_lights = self.spotLights,
@@ -38,7 +47,18 @@ class Scene:
                 print(f"Detail: {e}")
 
 class Camera:
-    def __init__(self, position = glm.vec3(0, 0, 1), target = glm.vec3(0), up = glm.vec3(0, 1, 0)):
+    def __init__(self, 
+                 position = glm.vec3(0, 0, 3), 
+                 target = glm.vec3(0, 0, -1), 
+                 up = glm.vec3(0, 1, 0)):
         self.position = position
         self.target = target
         self.up = up
+
+        self.yaw = -90.0
+        self.pitch = 0.0
+        self.sensitivity = 0.1
+        self.speed = 0.1
+
+    def get_view_matrix(self):
+        return glm.lookAt(self.position, self.position + self.target, self.up)
--- src/modules/window.py
+++ src/modules/window.py
@@ -1,4 +1,4 @@
-import glfw
+import glfw, glm
 from OpenGL.GL import *
 from modules.scene import Scene
 
@@ -15,20 +15,27 @@ class Window:
         self.monitor: glfw._GLFWmonitor = glfw.get_primary_monitor()
         self.vidmode: glfw._GLFWvidmode = glfw.get_video_mode(self.monitor)
         if fullscreen:
-            self.window: glfw._GLFWwindow = glfw.create_window(self.vidmode.size.width,
-                                                               self.vidmode.size.height,
+            self.width, self.height = self.vidmode.size.width, self.vidmode.size.height
+            self.window: glfw._GLFWwindow = glfw.create_window(self.width,
+                                                               self.height,
                                                                "Main",
                                                                self.monitor,
                                                                None)
         else:
-            self.window: glfw._GLFWwindow = glfw.create_window(width, height, "Main", None, None)
-        self.fullscreen = fullscreen
+            self.width, self.height = width, height
+            self.window: glfw._GLFWwindow = glfw.create_window(self.width, 
+                                                               self.height, 
+                                                               "Main", 
+                                                               None, 
+                                                               None)
+        self.fullscreen: bool = fullscreen
 
-        self.width = width
-        self.height = height
+        self.rotation_mode: bool = rotation_mode
+        self.animation_mode: bool = animation_mode
+        self.camera_speed: float = 0.0
 
-        self.rotation_mode = rotation_mode
-        self.animation_mode = animation_mode
+        self.x_last = self.width // 2
+        self.y_last = self.height // 2
 
         if not self.window:
             glfw.terminate()
@@ -36,6 +43,8 @@ class Window:
         
         glfw.make_context_current(self.window)
         glfw.swap_interval(1)
+
+        self.keys: list[bool] = [False] * 1024
         self.__set_callbacks()
 
         self.scene = Scene(aspect = self.width / self.height)
@@ -49,25 +58,84 @@ class Window:
     def __set_callbacks(self):
         glfw.set_window_size_callback(self.window, self.__window_size_callback)
         glfw.set_key_callback(self.window, self.__key_callback)
+        glfw.set_cursor_pos_callback(self.window, self.__mouse_callback)
+        glfw.set_scroll_callback(self.window, self.__scroll_callback)
 
     def __window_size_callback(self, window: glfw._GLFWwindow, width, height) -> None:
         self.scene.aspect = width / height
+        self.width = width
+        self.height = height
         glViewport(0, 0, width, height)
 
     def __key_callback(self, window: glfw._GLFWwindow, key, scancode, action, mods) -> None:
-        if glfw.PRESS == glfw.get_key(window, glfw.KEY_ESCAPE):
+        if action == glfw.PRESS and not self.keys[key]:
+            self.keys[key] = True
+        elif action == glfw.RELEASE and self.keys[key]:
+            self.keys[key] = False
+
+        camera_speed = self.scene.camera.speed * self.scene.delta_time
+        
+        if self.keys[glfw.KEY_ESCAPE]:
             glfw.set_window_should_close(window, True)
 
-        if glfw.PRESS == glfw.get_key(window, glfw.KEY_BACKSPACE):
+        if self.keys[glfw.KEY_BACKSPACE]:
             if self.fullscreen:
                 glfw.set_window_monitor(window, None, 100, 100, self.width, self.height, self.vidmode.refresh_rate)
             else:
                 glfw.set_window_monitor(window, self.monitor, 0, 0, self.vidmode.size.width, self.vidmode.size.height, self.vidmode.refresh_rate)
-
             self.fullscreen = not self.fullscreen
 
-        if glfw.PRESS == glfw.get_key(window, glfw.KEY_R):
+        if self.keys[glfw.KEY_R]:
             self.rotation_mode = not self.rotation_mode
 
-        if glfw.PRESS == glfw.get_key(window, glfw.KEY_SPACE):
+        if self.keys[glfw.KEY_SPACE]:
             self.animation_mode = not self.animation_mode
+
+        if self.keys[glfw.KEY_W]:
+            self.scene.camera.position += camera_speed * self.scene.camera.target
+
+        if self.keys[glfw.KEY_S]:
+            self.scene.camera.position -= camera_speed * self.scene.camera.target
+
+        if self.keys[glfw.KEY_A]:
+            self.scene.camera.position -= glm.normalize(
+                glm.cross(self.scene.camera.target, self.scene.camera.up)
+                ) * camera_speed
+
+        if self.keys[glfw.KEY_D]:
+            self.scene.camera.position += glm.normalize(
+                glm.cross(self.scene.camera.target, self.scene.camera.up)
+                ) * camera_speed
+            
+    def __mouse_callback(self, window: glfw._GLFWwindow, xpos: float, ypos: float):
+        front = glm.vec3(0)
+        if self.rotation_mode:
+            xoffset = xpos - self.x_last
+            yoffset = self.y_last - ypos
+            self.x_last = xpos
+            self.y_last = ypos
+
+            xoffset *= self.scene.camera.sensitivity
+            yoffset *= self.scene.camera.sensitivity
+
+            self.scene.camera.yaw += xoffset
+            self.scene.camera.pitch += yoffset
+
+            if self.scene.camera.pitch > 89.0:
+                self.scene.camera.pitch = 89.0
+            if self.scene.camera.pitch < -89.0:
+                self.scene.camera.pitch = -89.0
+
+        front.x = glm.cos(glm.radians(self.scene.camera.yaw)) * glm.cos(glm.radians(self.scene.camera.pitch))
+        front.y = glm.sin(glm.radians(self.scene.camera.pitch))
+        front.z = glm.sin(glm.radians(self.scene.camera.yaw)) * glm.cos(glm.radians(self.scene.camera.pitch))
+            
+        self.scene.camera.target = glm.normalize(front)
+
+    def __scroll_callback(self, window: glfw._GLFWwindow, xoffset: float, yoffset: float):
+        if self.scene.fov >= 1.0 and self.scene.fov <= 90.0:
+            self.scene.fov -= yoffset
+        self.scene.fov = 1.0 if self.scene.fov < 1.0 else \
+            90.0 if self.scene.fov > 90.0 else \
+                self.scene.fov
+        
